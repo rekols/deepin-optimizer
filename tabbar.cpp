f17124cd1680cab99388f6fe5f29c0a12c543084
rekols <rekols@foxmail.com>
2017-09-02 13:46:03 +0800

    add icon in tabbar

--- tabbar.cpp
+++ tabbar.cpp
@@ -1,11 +1,16 @@
 #include "tabbar.h"
+#include <QLabel>
 
 TabBar::TabBar(QWidget *parent) : QWidget(parent)
 {
     layout = new QHBoxLayout(this);
     tb = new QTabBar();
+    QLabel *icon = new QLabel();
+    icon->setPixmap(QPixmap(":/images/360logo.svg").scaled(22, 22));
 
-    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setMargin(0);
+    layout->addSpacing(5);
+    layout->addWidget(icon);
     layout->addStretch();
     layout->addWidget(tb);
     layout->addStretch();
