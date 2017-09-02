3f066d3530679de9b67e219e065865d5ad520bf5
rekols <rekols@foxmail.com>
2017-09-02 12:43:03 +0800

    Set window minimum size

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     MainWindow w;
+    w.setMinimumSize(800, 500);
     w.show();
     w.move((QApplication::desktop()->width() - w.width()) / 2,
            (QApplication::desktop()->height() - w.height()) / 2);
